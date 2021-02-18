89f7193cbb2f14940b853dce1f8e47df4a30fcad
Константин Дмитриевич Горбунов <264892@niuitmo.ru>
2021-02-18 12:22:24 +0300

    Stable version

--- WeatherAPI/Program.cs
+++ WeatherAPI/Program.cs
@@ -1,16 +1,16 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
+
 
 namespace WeatherAPI
 {
+    /// <summary>
+    /// Класс клиента
+    /// </summary>
     class Client
     {
         private WeatherInfo _weatherInfo;
@@ -20,139 +20,110 @@ namespace WeatherAPI
         }
         static void Main(string[] args)
         {
-            long a = DateTime.Now.Ticks;
-            Console.WriteLine(a);
-            Console.WriteLine(a.ToString().Length);
-            DateTime d = new DateTime(1568977200);
-            Console.WriteLine(d.ToString());
-            Console.WriteLine(new Client()._weatherInfo.Result());
+            Client client = new Client();
+            double lat, lon;
+            Console.WriteLine("Введите координаты");
+            Console.Write("Lat: ");
+            if (!double.TryParse(Console.ReadLine(), out lat))
+            {
+                throw new InvalidCastException();
+            }
+            Console.Write("Lon: ");
+            if (!double.TryParse(Console.ReadLine(), out lon))
+            {
+                throw new InvalidCastException();
+            }
+
+            var result = client._weatherInfo.GetWeather(lat, lon);
+
+            Console.WriteLine($"Прогноз утренней погоды в городе с координатами: {lat}, {lon}");
+            Console.WriteLine("Celsius | Date");
+            foreach (var elem in result)
+            {
+                Console.WriteLine(elem.MorningTemperature + " | " + elem.Date.ToString("dd.MM.yyyy"));
+
+            }
+            Console.WriteLine("**********************");
+            Console.WriteLine("Максимальная утренняя температура за 5 предстоящих дней: " + result.Select(x => x.MorningTemperature).Max() + " градус(ов)");
+            Console.WriteLine("Средняя утренняя температура за 5 предстоящих дней: " + result.Select(x => x.MorningTemperature).Average() + " градус(ов)");
         }
     }
+    /// <summary>
+    /// Класс предназначен для работы с API портала openweathermap.org
+    /// </summary>
     class WeatherInfo
     {
         public string Url { get; set; }
-        public string AppId{get;set; }
+        public string AppId { get; set; }
         public WeatherInfo()
         {
-            Url = "http://api.openweathermap.org/data/2.5/weather?";
-            AppId = "b465f78c20e5fc7797d3e86c60fb7842";
+            Url = "https://api.openweathermap.org/data/2.5/onecall?";
+            AppId = "here your app id";
         }
-        public string Result()
+
+        /// <summary>
+        /// Метод возвращает коллекцию, содержащую 5 объектов класса WeatherModel, соответствующих 5 дням
+        /// </summary>
+        /// <param name="lat">Широта</param>
+        /// <param name="lon">Долгота</param>
+        /// <returns></returns>
+
+        public List<WeatherModel> GetWeather(double lat, double lon)
         {
-            string url = $"https://api.openweathermap.org/data/2.5/forecast?q=London&cnt=3&units=metric&mode=xml&appid={AppId}";
-
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-
-            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-            string response;
-
-            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            try
             {
-                response = streamReader.ReadToEnd();
-            }
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(response);
-            XmlNodeList nodeList =(doc.SelectNodes("weatherdata/forecast/time/temperature"));
-            foreach (XmlNode elem in nodeList)
-            {
-                Console.WriteLine(elem.Attributes[1].Value );
-                
-            }
-            Console.WriteLine();
-            var s = doc.GetElementsByTagName("time");
-            foreach (XmlNode e in s) {
-                DateTime time = DateTime.Parse(e.Attributes[1].Value.ToString());
-                Console.WriteLine(time); }
-            return response;
-        }
-        private static void PrintItem(XmlElement item, int indent = 0)
-        {
-            // Выводим имя самого элемента.
-            // new string('\t', indent) - создает строку состоящую из indent табов.
-            // Это нужно для смещения вправо.
-            // Пробел справа нужен чтобы атрибуты не прилипали к имени.
-            Console.Write($"{new string('\t', indent)}{item.LocalName} ");
+                string url = Url + $"lat={lat}&lon={lon}&units=metric&appid={AppId}";
 
-            // Если у элемента есть атрибуты, 
-            // то выводим их поочередно, каждый в квадратных скобках.
-            foreach (XmlAttribute attr in item.Attributes)
-            {
-                Console.Write($"[{attr.InnerText}]");
-            }
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
 
-            // Если у элемента есть зависимые элементы, то выводим.
-            foreach (var child in item.ChildNodes)
-            {
-                
-                if (child is XmlElement node)
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                string response;
+
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
                 {
-                    if(node.InnerText=="temperature")Console.WriteLine(node.InnerText+"*******************");
-                    // Если зависимый элемент тоже элемент,
-                    // то переходим на новую строку 
-                    // и рекурсивно вызываем метод.
-                    // Следующий элемент будет смещен на один отступ вправо.
-                    Console.WriteLine();
-                    PrintItem(node, indent + 1);
+                    response = streamReader.ReadToEnd();
+                }
+                var json = JObject.Parse(response);
+                var data = json["daily"];
+                List<WeatherModel> result = new List<WeatherModel>();
+                for (int i = 0; i < 5; i++)
+                {
+                    if (result.Count == 0)
+                    {
+                        result.Add(new WeatherModel(lat, lon, DateTime.Now, (string)data[i]["temp"]["morn"]));
+                    }
+                    else
+                    {
+                        result.Add(new WeatherModel(lat, lon, result.Last<WeatherModel>().Date.AddDays(1), (string)data[i]["temp"]["morn"]));
+                    }
                 }
 
-                if (child is XmlText text)
-                {
-                    // Если зависимый элемент текст,
-                    // то выводим его через тире.
-                    Console.Write($"- {text.InnerText}");
-                }
+                return result;
             }
-        }
-            public WeatherResponse GetWeather(string city, int daysCount)
-        {
-            string url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=[your_app_id]";
-
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-
-            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-            string response;
-
-            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            catch (Exception e)
             {
-                response = streamReader.ReadToEnd();
+                Console.WriteLine(e.Message);
+                return new List<WeatherModel>();
             }
-
-            WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response);
-            return weatherResponse;
         }
-        public WeatherResponse GetWeather(string x, string y,int daysCount)
+    }
+    /// <summary>
+    /// Модель погоды
+    /// </summary>
+    class WeatherModel
+    {
+        public WeatherModel(double lat, double lon, DateTime date, string morningTemperature)
         {
-            string url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=[your_app_id]";
-
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-
-            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-            string response;
-
-            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
-            {
-                response = streamReader.ReadToEnd();
-            }
-
-            WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response);
-            return weatherResponse;
+            Lat = lat;
+            Lon = lon;
+            Date = date;
+            MorningTemperature = double.Parse(morningTemperature);
         }
-    }
-    public class Response
-    {
-        List<WeatherResponse> list { get; set; }
-    }
-    public class WeatherResponse
-    {
-        public TemperatureInfo Main { get; set; }
 
-        
-    }
-    public class TemperatureInfo
-    {
-        public float Temp { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public DateTime Date { get; set; }
+        public double MorningTemperature { get; set; }
     }
 }
